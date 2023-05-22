dccbc89b611ef183009597fe42017a915d6f7d24
ohansemmanuel <ohansemmanuel@outlook.com>
2023-05-22 11:17:55 +0200

    ✨ feat: add Vue component

---
+++ src/components/UpvoteContent.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    // data properties used in the UI template
+    return {
+      upvoteCount: 0,
+      maxUpvoteCount: 50,
+    };
+  },
+  methods: {
+    // method called when you click the upvote button
+    upvote() {
+      if (this.upvoteCount < this.maxUpvoteCount) {
+        this.upvoteCount++;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button @click="upvote">Upvote</button>
+    <div>
+      <div>Vue</div>
+
+      <div>{{ `${upvoteCount} upvotes` }}</div>
+      <div :style="{ width: `${upvoteCount}%` }" />
+      <div v-if="upvoteCount === maxUpvoteCount">Max upvote reached</div>
+    </div>
+  </div>
+</template>
